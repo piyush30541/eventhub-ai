9e9578584e59b20e4ded9c6fe74eb1997ef8a527
piyush30541 <155384241+piyush30541@users.noreply.github.com>
2026-01-01 18:00:17 +0530

    Update index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -3,7 +3,7 @@ export default function Home() {
     <div style={{ fontFamily: 'sans-serif', padding: '40px' }}>
       <h2>EventHub AI Application</h2>
       <p>This full-stack app is deployed using DevOps CI/CD pipeline.</p>
-      <p>Backend API URL: <a href="http://13.233.69.6:5000/api/message">http://YOUR_PUBLIC_IP:5000/api/message</a></p>
+      <p>Backend API URL: <a href="http://13.233.69.6:5000/api/message">http://13.233.69.6:5000/api/message</a></p>
     </div>
   );
 }
